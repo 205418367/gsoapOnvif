85996f4499f2385d262fd528cf1c0f7938200387
205418367 <47652945+205418367@users.noreply.github.com>
2020-07-28 16:54:46 +0800

    Update onvifdevice.h

--- onvifdevice.h
+++ onvifdevice.h
@@ -8,26 +8,39 @@
 #include <string>
 #include <vector>
 
+//控制方位
 #define UP 0
 #define DOWN 1
 #define LEFT 2
 #define RIGHT 3
-#define ZOOMIN 4
-#define ZOOMOUT 5
-#define LEFTUP 6
-#define LEFTDOWN 7
-#define RIGHTUP 8
-#define RIGHTDOWN 9
+#define LEFTUP 4
+#define LEFTDOWN 5
+#define RIGHTUP 6
+#define RIGHTDOWN 7
+#define ZOOMIN 8
+#define ZOOMOUT 9
 
+//控制速度
 #define LOWER 1
 #define LOW 2
 #define MEDIUM 3
 #define FAST 4
 #define FASTER 5
 
+//预置点位
 #define GOTO 0
 #define SET 1
 #define REMOVE 2
+#define PRESET1 "1"
+#define PRESET2 "2"
+#define PRESET3 "3"
+#define PRESET4 "4"
+#define PRESET5 "5"
+#define PRESET6 "6"
+#define PRESET7 "7"
+#define PRESET8 "8"
+#define PRESET9 "9"
+#define PRESET10 "10"
 
 #define MAX_HOSTNAME_LEN 128
 #define MAX_LOGMSG_LEN 256 
@@ -54,7 +67,7 @@ public:
 /************************************************************************
 **函数：ptzContinuousMove
 **功能：控制相机持续转动
-command：UP DOWN LEFT RIGHT LEFTUP LEFTDOWN RIGHTUP RIGHTDOWN ZOOMIN ZOOMOUT
+**command：UP DOWN LEFT RIGHT ZOOMIN ZOOMOUT
 ************************************************************************/
   int ptzContinuousMove(int command,int speed);
 
@@ -68,10 +81,11 @@ command：UP DOWN LEFT RIGHT LEFTUP LEFTDOWN RIGHTUP RIGHTDOWN ZOOMIN ZOOMOUT
   int getProfile(string& profileToken);
   int getRTSPUrl(string& rtspUrl);
   int getIMAGEUrl(string& imageUrl);
-  /************************************************************************
+/************************************************************************
 **函数：ptzPreset
 **功能：控制预置位
-presettoken: GOTO SET REMOVE
+command: GOTO SET REMOVE
+presettoken:PRESET1 PRESET2 ... PRESET10
 ************************************************************************/
   int ptzPreset(int command, string presettoken);
   int ptzPresetTour(vector<string> &PresetToken);
